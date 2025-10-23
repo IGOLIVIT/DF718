9f6de58324cf01f5776078787e9f6c232731fc70
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2025-10-23 22:09:27 +0300

    new5

--- DF718/ContentView.swift
+++ DF718/ContentView.swift
@@ -52,7 +52,7 @@ struct ContentView: View {
     
     private func check_data() {
         
-        let lastDate = "25.10.2025"
+        let lastDate = "28.10.2025"
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy"
